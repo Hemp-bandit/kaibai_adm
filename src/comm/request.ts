17d84eb972aef853ac687ae9e0706cb4ab94c9c8
wyswill <www.1151373571@live.com>
2024-11-27 18:25:16 +0800

    feat: 代码优化

--- src/comm/request.ts
+++ src/comm/request.ts
@@ -2,8 +2,10 @@ import axios, {AxiosResponse} from 'axios';
 import {createDiscreteApi} from "naive-ui";
 import login_tool from "@/comm/login_tool";
 
+// @ts-ignore
+const isDev = process.env.NODE_ENV === "development"
 const instance = axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL:  isDev ? "http://localhost:3000/api" : "http://1.94.186.245:30000/api"
     // baseURL: "http://1.94.186.245:30000/api"
 })
 const is_login = login_tool.check_is_login_local();
